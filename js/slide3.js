9bf43a59aeaa84b47e92654049d5a322df37c6cf
David Orona <oronadavid@gmail.com>
2024-07-28 01:54:45 -0500

    Revert "Remove $0 data points"
    This reverts commit a4701994ca940caab2590ee6802667b1bcc7cc4d.

--- js/slide3.js
+++ js/slide3.js
@@ -1,68 +1,51 @@
-d3.csv("data/ev_car_data.csv").then(data => {
-    const filteredData = data.filter(d => d["Base MSRP"] > 0);
-    const carModels = filteredData.map(d => d.Model);
-    const evTypes = filteredData.map(d => d["Electric Vehicle Type"]);
-    const counts = {};
-    
-    carModels.forEach((model, i) => {
-        const type = evTypes[i];
-        if (!counts[model]) {
-            counts[model] = { count: 0, type: type };
-        }
-        counts[model].count += 1;
-    });
+const graphics3 = new Graphics(600, 400);
+graphics3.createSVG('#slide3');
+graphics3.createTooltip();
 
-    const processedData = Object.keys(counts).map(model => ({
-        model: model,
-        count: counts[model].count,
-        type: counts[model].type
-    }));
+Promise.all([
+    d3.csv("data/ev_car_data.csv"),
+    d3.csv("data/non_ev_car_data.csv")
+]).then(([evData, nonEvData]) => {
+    const x = d3.scaleLinear().domain([0, d3.max(nonEvData, d => +d.price)]).range([0, graphics3.width]);
+    const y = d3.scaleLinear().domain([0, d3.max(nonEvData, d => +d.horsepower)]).range([graphics3.height, 0]);
 
-    const width = 800;
-    const height = 400;
-    const margin = { top: 20, right: 30, bottom: 40, left: 90 };
+    graphics3.svg.append("g")
+        .attr("transform", `translate(0, ${graphics3.height})`)
+        .call(d3.axisBottom(x));
 
-    const svg = d3.select("#bar-chart")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.1)
-        .domain(processedData.map(d => d.model));
-
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end");
-
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(processedData, d => d.count)])
-        .nice()
-        .range([height, 0]);
-
-    svg.append("g")
+    graphics3.svg.append("g")
         .call(d3.axisLeft(y));
 
-    svg.selectAll(".bar")
-        .data(processedData)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.model))
-        .attr("y", d => y(d.count))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d.count))
-        .attr("fill", "#69b3a2")
-        .on("mouseover", function (event, d) {
-            const [x, y] = d3.pointer(event);
-            const html = `<strong>Model:</strong> ${d.model}<br><strong>Count:</strong> ${d.count}<br><strong>Type:</strong> ${d.type}`;
-            graphics.showTooltip(html, x, y);
+    graphics3.svg.selectAll("circle.non-ev")
+        .data(nonEvData)
+        .enter().append("circle")
+        .attr("class", "non-ev")
+        .attr("cx", d => x(+d.price))
+        .attr("cy", d => y(+d.horsepower))
+        .attr("r", 3)
+        .attr("fill", "red")
+        .on("mouseover", (event, d) => {
+            graphics3.showTooltip(`Price: $${d.price}<br>Horsepower: ${d.horsepower}`, event.pageX, event.pageY);
         })
-        .on("mouseout", () => graphics.hideTooltip());
+        .on("mouseout", () => {
+            graphics3.hideTooltip();
+        });
+
+    graphics3.svg.selectAll("circle.ev")
+        .data(evData)
+        .enter().append("circle")
+        .attr("class", "ev")
+        .attr("cx", d => x(+d["Base MSRP"]))
+        .attr("cy", d => y(+d["Electric Range"]))
+        .attr("r", 3)
+        .attr("fill", "blue")
+        .on("mouseover", (event, d) => {
+            graphics3.showTooltip(`Price: $${d["Base MSRP"]}<br>Range: ${d["Electric Range"]} miles`, event.pageX, event.pageY);
+        })
+        .on("mouseout", () => {
+            graphics3.hideTooltip();
+        });
+
+    graphics3.addAxisLabel('x', 'Price ($)');
+    graphics3.addAxisLabel('y', 'Horsepower (Non-EV) / Range (EV)');
 });
